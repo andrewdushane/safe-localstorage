74fa2da61940eedf364cb98b45118342dbbf91ad
AJD <ajd@fl-tdbrch-5075.partners.lan>
2017-08-31 09:57:28 -0400

    Initial commit

---
+++ src.js
@@ -0,0 +1,42 @@
+const localStorageAvailable = () => {
+  const inWindow =
+    typeof window.localStorage === 'object' &&
+    typeof window.localStorage.setItem === 'function';
+  if (!inWindow) {
+    return false;
+  }
+  // Safari private mode has localStorage in the window, but throws when `setItem` is called
+  const key = 'safeLocalStorageTest';
+  try {
+    window.localStorage.setItem(key, 'succeeds');
+    window.localStorage.removeItem(key);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const noop = () => {};
+
+const useStorageWhenAvailable = (nativeMethod, args, callback = noop) => {
+  if (localStorageAvailable()) {
+    return window.localStorage[nativeMethod](...args);
+  } else {
+    callback();
+  }
+};
+
+export default {
+  get: (key, onLocalStorageNotAvailable) => {
+    return useStorageWhenAvailable('getItem', [key], onLocalStorageNotAvailable);
+  },
+  set: (key, value, onLocalStorageNotAvailable) => {
+    useStorageWhenAvailable('setItem', [key, value], onLocalStorageNotAvailable);
+  },
+  remove: (key, onLocalStorageNotAvailable) => {
+    useStorageWhenAvailable('removeItem', [key], onLocalStorageNotAvailable);
+  },
+  removeAll: (onLocalStorageNotAvailable) => {
+    useStorageWhenAvailable('clear', [], onLocalStorageNotAvailable);
+  },
+};
